8f25f9c427fd58d8ae7af773b09a3522e6a4377a
Adwoa Acheampong <adwoaacheampong728@gmail.com>
2026-02-14 12:36:17 +0000

    ...1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useEffect } from 'react';
+import React, { useState, lazy, Suspense, useEffect } from 'react';
 import ErrorBoundary from './components/ErrorBoundary';
 import NavBar from './components/NavBar';
 import ContactForm from './components/ContactForm';
